e8fd5e5e7a72966f67d430ff94d4ce7f4d630c7c
mihai_raevschi <mihairaevschi001@gmail.com>
2021-02-22 03:28:23 +0200

    Laborator 6

---
+++ Laborator 6/app/src/main/java/com/example/laboratorcolegiu/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.laboratorcolegiu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
+public class MainActivity<override> extends AppCompatActivity {
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView=(ListView)findViewById(R.id.listview);
+        ArrayList arrayList= new ArrayList();
+        arrayList.add("Hello World!");
+        arrayList.add("Lorem Ipsum is simply dummy text of the printing and typesetting industry.");
+        arrayList.add("It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.");
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    public void displayToast(View view) {
+        Toast.makeText(MainActivity.this, "Toast", Toast.LENGTH_SHORT).show();
+    }
+    public void displaySnackbar(View view) {
+        Snackbar.make(findViewById(R.id.rootLayout),"Snackbar",  Snackbar.LENGTH_SHORT)
+                .setAction("CLOSE", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }).show();
+    }
+    public void displayPopup(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you Sure?")
+                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+        // Create the AlertDialog object and return it
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    public void displayShare(View view) {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+        String sharebody = "Hello World!";
+        String subject = "Subiectul";
+        i.putExtra(Intent.EXTRA_SUBJECT, sharebody);
+        i.putExtra(Intent.EXTRA_TEXT, subject);
+        startActivity(Intent.createChooser(i, "Title"));
+    }
+}
